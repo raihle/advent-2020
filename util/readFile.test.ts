4f5ddf7c8358e05afbafc2a207d037fc815d1f4c
Jacob Raihle <jraihle@gmail.com>
2020-12-06 15:03:05 +0100

    Clean up file reading utils

--- util/readFile.test.ts
+++ util/readFile.test.ts
@@ -1,21 +1,29 @@
-import { linesAsArray } from "./readFile";
+import { linesAsArray, blocksAsArray } from "./readFile";
 
-describe("Readfile", () => {
-  describe("Array", () => {
-    describe("Reads all lines of a file and returns them as an array of strings", () => {
-      it("For files with terminating new-lines", () => {
-        expect(linesAsArray("util/input-with-final-newline.txt")).toEqual([
-          "a",
-          "b",
-        ]);
-      });
-
-      it("For files without terminating new-lines", () => {
-        expect(linesAsArray("util/input-without-final-newline.txt")).toEqual([
-          "a",
-          "b",
-        ]);
-      });
+describe("linesAsArray", () => {
+  describe("Reads all lines of a file and returns them as an array of strings", () => {
+    it("For files with terminating new-lines", () => {
+      expect(linesAsArray("util/input-with-final-newline.txt")).toEqual([
+        "a",
+        "b",
+      ]);
     });
+
+    it("For files without terminating new-lines", () => {
+      expect(linesAsArray("util/input-without-final-newline.txt")).toEqual([
+        "a",
+        "b",
+      ]);
+    });
+  });
+});
+
+describe("blocksAsArray", () => {
+  it("Reads all lines of a file and returns blocks separated by an empty line, with intra-block newlines preserved", () => {
+    expect(blocksAsArray("util/input-with-blocks.txt")).toEqual([
+      "a\nb",
+      "cd",
+      "e\nf",
+    ]);
   });
 });
--- util/readFile.ts
+++ util/readFile.ts
@@ -5,23 +5,23 @@ export const linesAsArray = function (filename) {
   const contents = readFileSync(resolve(process.cwd(), filename), "utf8").split(
     "\n"
   );
-  if (contents[contents.length - 1] === "" && contents.length > 1) {
-    return contents.slice(0, contents.length - 1);
-  }
-  return contents;
+  return trimWhiteSpace(contents);
 };
 
 export const blocksAsArray = function (filename) {
-  const lines = linesAsArray(filename);
-  return lines.reduce(
-    (blocks, line) => {
-      if (line === "") {
-        blocks.push("");
-      } else {
-        blocks[blocks.length - 1] += line + "\n";
-      }
-      return blocks;
-    },
-    [""]
+  const contents = readFileSync(resolve(process.cwd(), filename), "utf8").split(
+    "\n\n"
   );
+
+  return trimWhiteSpace(contents);
 };
+
+function trimWhiteSpace(lines) {
+  return trimTerminatingNewLine(lines.map((line) => line.trim()));
+}
+function trimTerminatingNewLine(lines) {
+  if (lines[lines.length - 1] === "") {
+    return lines.slice(0, lines.length - 1);
+  }
+  return lines;
+}
